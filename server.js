bc1ef61798e12f73bb2a55169ba2fd5d1c56bb45
Sanket Debnath <sanketdebnath24@gmail.com>
2021-04-24 11:54:09 +0530

    "Added Style guide"

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
 const PORT = 3000;
 
-app.use("/", express.static("public"));
+app.use('/', express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-app.get("/", (req, res) => {
-  res.send("Testing");
+app.get('/', (req, res) => {
+  res.send('Testing');
 });
 app.listen(PORT, console.log(`Server running on ${PORT}`));
